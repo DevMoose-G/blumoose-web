947837db85b6fd09beffacbd07080facc0e74f10
DevMoose-G <muse.gimicael@gmail.com>
2022-03-19 22:32:05 -0400

    Minor change

--- email.php
+++ email.php
@@ -1,8 +1,13 @@
 <?php
 
-if(isset($_POST['sname'])){
+if(isset($_POST['sname'], $_POST['task'])){
     $retval = mail("muse.gimicael@gmail.com", "New Task From"+$_POST['sname'], 
         $_POST['task']);
+    if( $retval == true ) {
+        echo "Message sent successfully...";
+    } else {
+        echo "Message could not be sent...";
+    }
 }
 header("Location:".$_SERVER[HTTP_REFERER]);
 die;
